7c9af03cf9ba200e44bb7bb18d687b7b04ec41c3
jcc <jccollart@outlook.be>
2023-04-14 11:05:14 +0200

    api-user

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
-fetch("data.json").then((res) => {
-  res.json().then((data) => console.log(data));
-});
+// // fetch("data.json").then((res) => {
+// //   res.json().then((data) => console.log(data));
+// // });
+
+// const init2 = {
+//   method: "DELETE",
+//   headers: {
+//     "content-type": "application/json",
+//   },
+//   // body: JSON.stringify({
+//   //   pseudo: "jcc70",
+//   //   message: "hellooooo!!",
+//   // }),
+//   mode: "cors",
+//   Credentials: "same-origin",
+// };
+// document.querySelector("form").addEventListener("submit", () => {
+//   fetch("http://localhost:3000/posts/2", init2).then(() =>
+//     console.log("data envoyée")
+//   );
+// });
